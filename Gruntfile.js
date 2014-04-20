66b14a81bc995dcaf8624d41c12be0d81ab5da3a
James Kyle <me@thejameskyle.com>
2014-04-20 13:49:45 -0400

    Add Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    version: '<%= pkg.version %>'
+  });
+
+  grunt.registerTask('default', []);
+
+};
